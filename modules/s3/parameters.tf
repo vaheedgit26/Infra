960b6cebdaf29f71481c90e5a69c4d8b1a47eb78
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-17 09:37:13 +0530

    Create parameters.tf

---
+++ modules/s3/parameters.tf
@@ -0,0 +1,29 @@
+# Storing parameters in AWS SSM PARAMETER STORE 
+
+resource "aws_ssm_parameter" "project_name" {
+  type  = "String"
+  name  = "/project_name/${var.project_name}"
+  value = var.project_name
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "region" {
+  type  = "String"
+  name  = "/${var.project_name}/region/${var.region}"
+  value = var.region
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "env" {
+  type  = "String"
+  name  = "/${var.project_name}/env/${var.env}"
+  value = var.env
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "tfstate_bucket" {
+  type  = "String"
+  name  = "/tfstate_bucket/${var.project_name}/${var.region}"
+  value = aws_s3_bucket.tfstate_bucket.id
+  overwrite = true
+}
