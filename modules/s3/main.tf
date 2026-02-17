9992cd53c8bb269cdf3a2b4a149145d4b6e622da
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-17 20:43:21 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,6 +6,7 @@
 
 resource "aws_s3_bucket" "bucket" {
   # bucket = "${var.s3_bucket_name}-${random_string.suffix.result}"
+
   bucket = var.s3_bucket_name
   force_destroy = true   # will be deleted forcefully
 
