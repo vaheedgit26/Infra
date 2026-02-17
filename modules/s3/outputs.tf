92e55f1a0cc3428ceb28420736fec8261d7ef44e
vaheedgit26 <vaheed.aws@gmail.com>
2026-02-17 18:24:33 +0530

    Update outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,9 +1,9 @@
-output "tfstate_bucket_arn" {
+output "bucket_arn" {
   description = "ARN of the Terraform remote state S3 bucket"
-  value       = aws_s3_bucket.tfstate_bucket.arn
+  value       = aws_s3_bucket.bucket.arn
 }
 
-output "tfstate_bucket_id" {
+output "bucket_id" {
   description = "Bucket ID (same as name) for Terraform state"
-  value       = aws_s3_bucket.tfstate_bucket.id
+  value       = aws_s3_bucket.bucket.id
 }
