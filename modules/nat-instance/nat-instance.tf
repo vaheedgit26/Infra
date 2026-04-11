ebd135f56e9744091271c28a1bfcd3953f57b102
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-11 20:17:10 +0530

    Update nat-instance.tf

--- modules/nat-instance/nat-instance.tf
+++ modules/nat-instance/nat-instance.tf
@@ -2,6 +2,7 @@ module "ec2" {
   source = "git::https://github.com/vaheedgit26/Infra.git//modules/ec2"
 
   ami_id                      = var.ami_id
+  instance_type               = var.instance_type
   public_key_name             = var.public_key_name
   sg_ids                      = [module.nat_sg.sg_id]
   subnet_id                   = var.public_subnet_ID_to_launch_nat_instance  # "subnet-088e8443a70102e2a" #1a
