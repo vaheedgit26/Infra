315a6687dee3938dd94ae29544cf67527f54beee
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-11 09:04:22 +0530

    Update nat-instance.tf

--- modules/nat-instance/nat-instance.tf
+++ modules/nat-instance/nat-instance.tf
@@ -7,7 +7,8 @@ module "ec2" {
   subnet_id                   = var.public_subnet_ID_to_launch_nat_instance  # "subnet-088e8443a70102e2a" #1a
   associate_public_ip_address = true
   is_nat_instance             = var.is_nat_instance  # creates NAT instance if true
-  # user_data                   = file("${path.module}/nat_user_data.sh")
+  is_eip_required             = var.is_eip_required
+  # user_data                 = file("${path.module}/nat_user_data.sh")
 
   project_name                = var.project_name
   env                         = var.env
