3c669c305f08751ef8b0e62dcd87a78df6ad2c06
vaheedgit26 <vaheed.aws@gmail.com>
2026-04-13 11:43:51 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -3,7 +3,7 @@
 ################################################################
 variable "ami_id" {}
 variable "public_key_name" {}
-variable "sg_ids" {}
+variable "sg_ids" { type = list(string) }
 variable "subnet_id" {}
 
 variable "project_name" {}
